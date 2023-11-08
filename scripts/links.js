d89287699b79f1117b5e70830f68868dd80081e3
runyun <runningyun@gmail.com>
2023-11-08 22:33:46 +0800

    Add data in the json file.

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,13 @@
 const baseURL = "https://runyun.github.io/wdd230";
 const linksURL = "https://runyun.github.io/wdd230/data/links.json";
+
+async function getData(url){
+    let response = await fetch(url);
+
+    if(response.ok){
+        let data = await response.json();
+        console.table(data);
+    }
+}
+
+getData(linksURL);
