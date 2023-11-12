9e82fd92cd05a7a1f3d1cc9eb8ea223e51dec2be
runyun <runningyun@gmail.com>
2023-11-12 20:48:34 +0800

    Add the Weather API assignment.

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,51 @@
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const key = '98185f587a5e31720e806dd1a3bbbe98';
+const lat = 49.75;
+const lon = 6.64;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        
+        if (response.ok) {
+            const data = await response.json();
+
+            displayResults(data);
+
+        }else {
+            throw Error(await response.text());
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.textContent = `${Math.round(data.main.temp)} ℉`;
+    weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    weatherIcon.alt = `${data.weather[0].description}`;
+    captionDesc.textContent = `${captalize(data.weather[0].description)}`;
+}
+
+function captalize(words){
+    const wordsArray = words.split(' ');
+
+    let result = '';
+    wordsArray.forEach(word => {
+       const captalizeWord = word[0].toUpperCase() + word.substring(1); 
+       result += ' ' + captalizeWord;
+    });
+
+    return result;
+}
+
+
+
+
+apiFetch();
