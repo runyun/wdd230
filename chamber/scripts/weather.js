1388a250bef73ad184352733df071472975f5cb7
runyun <runningyun@gmail.com>
2023-11-19 08:20:06 +0800

    Change weather week name from h3 to h4. Add alt and src of the weather icon.

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -105,7 +105,7 @@ function renderForeCast(data) {
     const imgalt = `${data.weather[0].description}`;
 
     const section = document.createElement('section');
-    const week = document.createElement('h3');
+    const week = document.createElement('h4');
     const weatherIcon = document.createElement('img');
     const temp = document.createElement('p');
 
